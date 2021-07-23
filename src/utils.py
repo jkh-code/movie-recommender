71c19736017ecd1a507bff6caf2befe8333ceec3
zsnyder21 <64924773+zsnyder21@users.noreply.github.com>
2021-07-22 19:56:25 -0600

    Added more cleaning functions to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,35 @@
 import ast
-
 import numpy as np
 import pandas as pd
+import re
+
+
+def stringify(obj: str, name: str = "name", sep: str = ", ") -> str:
+    """
+    This function takes a string representations of a list of dictionaries
+    and turns a specified attribute into a delimited string of those
+    attributes.
+
+    :param obj: String representation of object
+    :param name: Name of the key in the dictionary to extract
+    :param sep: What to delimit the string with
+    :return: Delimited string
+    """
+    dicts = ast.literal_eval(obj)
+    if type(dicts) is list:
+        return sep.join(d[name] for d in ast.literal_eval(obj) if type(d) is dict)
+    else:
+        return np.NaN
 
 
 def clean_meta_data(df: pd.DataFrame, in_place: bool = False) -> pd.DataFrame:
-    def stringify(obj: str, name: str = "name", sep: str = ", ") -> str:
-        dicts = ast.literal_eval(obj)
-        if type(dicts) is list:
-            return sep.join(d[name] for d in ast.literal_eval(obj) if type(d) is dict)
-        else:
-            return np.NaN
+    """
+    Cleans the dataframe obtained from data/movies_metadata.csv
 
+    :param df: DataFrame object obtained from pd.read_csv()
+    :param in_place: Whether or not to return the cleaned df in place or not
+    :return: Cleaned pandas DataFrame object
+    """
     if not in_place:
         new_df = df.copy()
     else:
@@ -34,5 +52,48 @@ def clean_meta_data(df: pd.DataFrame, in_place: bool = False) -> pd.DataFrame:
 
     # Spoken Languages
     new_df["spoken_languages"] = new_df["spoken_languages"].map(lambda x: stringify(obj=x) if x is not np.NaN else np.NaN)
+
+    return new_df
+
+
+def clean_keywords(df: pd.DataFrame, in_place: bool = False) -> pd.DataFrame:
+    """
+    Cleans the dataframe obtained from data/keywords.csv
+
+    :param df: DataFrame object obtained from pd.read_csv()
+    :param in_place: Whether or not to return the cleaned df in place or not
+    :return: Cleaned pandas DataFrame object
+    """
+    if not in_place:
+        new_df = df.copy()
+    else:
+        new_df = df
+
+    new_df["keywords"] = new_df["keywords"].map(lambda x: stringify(obj=x) if x is not np.NaN else np.NaN)
+
+    return new_df
+
+
+def clean_movies(df: pd.DataFrame, in_place: bool = False) -> pd.DataFrame:
+    """
+    Cleans the dataframe obtained from data/movies.dat
+
+    :param df: DataFrame object obtained from pd.read_csv()
+    :param in_place: Whether or not to return the cleaned df in place or not
+    :return: Cleaned pandas DataFrame object
+    """
+    if not in_place:
+        new_df = df.copy()
+    else:
+        new_df = df
+
+    # Extract the release year from the title
+    new_df["Release"] = new_df["Title"].map(lambda x: re.findall(pattern=r"\d{4}", string=x)[0]).astype(int)
+
+    # Remove the release year from the title
+    new_df["Title"] = new_df["Title"].map(lambda x: re.sub(pattern=r"\s+\(\d{4}\)", string=x, repl=""))
+
+    # Split genres into a comma delimited list
+    new_df["Genres"] = new_df["Genres"].map(lambda x: ", ".join(x.split("|")))
 
     return new_df
